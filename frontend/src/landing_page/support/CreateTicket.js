3c85a3a80d243f8a4a9e5533dfb7641cf7a99101
Arpit Singh <singharpit0911@gmail.com>
2025-07-01 18:33:10 +0530

    create ticket UI setup

--- frontend/src/landing_page/support/CreateTicket.js
+++ frontend/src/landing_page/support/CreateTicket.js
@@ -1,7 +1,284 @@
-import React from 'react'
+import React from "react";
 
 export const CreateTicket = () => {
   return (
-    <div>CreateTicket</div>
-  )
-}
+    <div className="container pb-5">
+      <h1 className="text-muted fs-4 my-5">
+        To create a ticket, select a relevant topic
+      </h1>
+      <div className="row">
+        <div className="col-4">
+          <div>
+            <h1 className="fs-5">
+              <i class="fa fa-plus-circle" aria-hidden="true"></i>
+              &nbsp;&nbsp;Account Opening
+            </h1>
+            <div className="row ms-3">
+              <a
+                href="#"
+                className="text-decoration-none mt-2"
+                style={{ fontSize: "0.9rem" }}
+              >
+                Resident individual
+              </a>
+              <a
+                href="#"
+                className="text-decoration-none mt-2"
+                style={{ fontSize: "0.9rem" }}
+              >
+                Minor
+              </a>
+              <a
+                href="#"
+                className="text-decoration-none mt-2"
+                style={{ fontSize: "0.9rem" }}
+              >
+                Non Resident Indian (NRI)
+              </a>
+              <a
+                href="#"
+                className="text-decoration-none mt-2"
+                style={{ fontSize: "0.9rem" }}
+              >
+                Company, Partnership, HUF and LLP
+              </a>
+              <a
+                href="#"
+                className="text-decoration-none mt-2"
+                style={{ fontSize: "0.9rem" }}
+              >
+                Glossary
+              </a>
+            </div>
+          </div>
+          <div className="mt-5">
+            <h1 className="fs-5">
+              <i class="fa fa-credit-card" aria-hidden="true"></i>
+              &nbsp;&nbsp;Funds
+            </h1>
+            <div className="row ms-3">
+              <a
+                href="#"
+                className="text-decoration-none mt-2"
+                style={{ fontSize: "0.9rem" }}
+              >
+                Add money
+              </a>
+              <a
+                href="#"
+                className="text-decoration-none mt-2"
+                style={{ fontSize: "0.9rem" }}
+              >
+                Withdraw money
+              </a>
+              <a
+                href="#"
+                className="text-decoration-none mt-2"
+                style={{ fontSize: "0.9rem" }}
+              >
+                Add bank accounts
+              </a>
+              <a
+                href="#"
+                className="text-decoration-none mt-2"
+                style={{ fontSize: "0.9rem" }}
+              >
+                eMandates
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col-4">
+          <div>
+            <h1 className="fs-5">
+              <i class="fa fa-user-o" aria-hidden="true"></i>
+              &nbsp;&nbsp;Your Zerodha Account
+            </h1>
+            <div className="row ms-3">
+              <a
+                href="#"
+                className="text-decoration-none mt-2"
+                style={{ fontSize: "0.9rem" }}
+              >
+                Your Profile
+              </a>
+              <a
+                href="#"
+                className="text-decoration-none mt-2"
+                style={{ fontSize: "0.9rem" }}
+              >
+                Account modification
+              </a>
+              <a
+                href="#"
+                className="text-decoration-none mt-2"
+                style={{ fontSize: "0.9rem" }}
+              >
+                Client Master Report (CMR) and Depository Participant (DP)
+              </a>
+              <a
+                href="#"
+                className="text-decoration-none mt-2"
+                style={{ fontSize: "0.9rem" }}
+              >
+                Nomination
+              </a>
+              <a
+                href="#"
+                className="text-decoration-none mt-2"
+                style={{ fontSize: "0.9rem" }}
+              >
+                Transfer and conversion of securities
+              </a>
+            </div>
+          </div>
+          <div className="mt-5">
+            <h1 className="fs-5">
+              <i class="fa fa-circle-o-notch" aria-hidden="true"></i>
+              &nbsp;&nbsp;Console
+            </h1>
+            <div className="row ms-3">
+              <a
+                href="#"
+                className="text-decoration-none mt-2"
+                style={{ fontSize: "0.9rem" }}
+              >
+                Portfolio
+              </a>
+              <a
+                href="#"
+                className="text-decoration-none mt-2"
+                style={{ fontSize: "0.9rem" }}
+              >
+                Corporate actions
+              </a>
+              <a
+                href="#"
+                className="text-decoration-none mt-2"
+                style={{ fontSize: "0.9rem" }}
+              >
+                Funds statement
+              </a>
+              <a
+                href="#"
+                className="text-decoration-none mt-2"
+                style={{ fontSize: "0.9rem" }}
+              >
+                Reports
+              </a>
+              <a
+                href="#"
+                className="text-decoration-none mt-2"
+                style={{ fontSize: "0.9rem" }}
+              >
+                Profile
+              </a>
+              <a
+                href="#"
+                className="text-decoration-none mt-2"
+                style={{ fontSize: "0.9rem" }}
+              >
+                Segments
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col-4">
+          <div>
+            <h1 className="fs-5">
+              <i class="fa fa-bar-chart" aria-hidden="true"></i>
+              &nbsp;&nbsp;Kite
+            </h1>
+            <div className="row ms-3">
+              <a
+                href="#"
+                className="text-decoration-none mt-2"
+                style={{ fontSize: "0.9rem" }}
+              >
+                IPO
+              </a>
+              <a
+                href="#"
+                className="text-decoration-none mt-2"
+                style={{ fontSize: "0.9rem" }}
+              >
+                Trading FAQs
+              </a>
+              <a
+                href="#"
+                className="text-decoration-none mt-2"
+                style={{ fontSize: "0.9rem" }}
+              >
+                Margin Trading Facility (MTF) and Margins
+              </a>
+              <a
+                href="#"
+                className="text-decoration-none mt-2"
+                style={{ fontSize: "0.9rem" }}
+              >
+                Charts and orders
+              </a>
+              <a
+                href="#"
+                className="text-decoration-none mt-2"
+                style={{ fontSize: "0.9rem" }}
+              >
+                Alerts and Nudges
+              </a>
+              <a
+                href="#"
+                className="text-decoration-none mt-2"
+                style={{ fontSize: "0.9rem" }}
+              >
+                General
+              </a>
+            </div>
+          </div>
+          <div className="mt-5">
+            <h1 className="fs-5">
+              <i class="fa fa-circle-o" aria-hidden="true"></i>
+              &nbsp;&nbsp;Coin
+            </h1>
+            <div className="row ms-3">
+              <a
+                href="#"
+                className="text-decoration-none mt-2"
+                style={{ fontSize: "0.9rem" }}
+              >
+                Mutual funds
+              </a>
+              <a
+                href="#"
+                className="text-decoration-none mt-2"
+                style={{ fontSize: "0.9rem" }}
+              >
+                National Pension Scheme (NPS)
+              </a>
+              <a
+                href="#"
+                className="text-decoration-none mt-2"
+                style={{ fontSize: "0.9rem" }}
+              >
+                Features on Coin
+              </a>
+              <a
+                href="#"
+                className="text-decoration-none mt-2"
+                style={{ fontSize: "0.9rem" }}
+              >
+                Payments and Orders
+              </a>
+              <a
+                href="#"
+                className="text-decoration-none mt-2"
+                style={{ fontSize: "0.9rem" }}
+              >
+                General
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
