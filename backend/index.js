221fe3ef1d4b537a48ddc4aa1e430427467c5e0d
Arpit Singh <singharpit0911@gmail.com>
2025-08-02 11:20:22 +0530

    initial setup backend

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3002;
+const uri = process.env.MONGO_URL;
+
+const app = express();
+
+app.listen(PORT, () => {
+  console.log("Server listening to port 3002");
+  mongoose.connect(uri);
+  console.log("DB connected");
+});
