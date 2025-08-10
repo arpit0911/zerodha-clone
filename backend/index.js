a4a70d630eb95d9ff9db267cd407b62514c536ef
Arpit Singh <singharpit0911@gmail.com>
2025-08-10 11:34:31 +0530

    new order route

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ const bodyParser = require("body-parser");
 
 const { HoldingsModel } = require("./model/HoldingsModel");
 const { PositionsModel } = require("./model/PositionsModel");
+const { OrdersModel } = require("./model/OrdersModel");
 
 const PORT = process.env.PORT || 3002;
 const uri = process.env.MONGO_URL;
@@ -192,6 +193,19 @@ app.get("/allPositions", async (req, res) => {
   let allPositions = await PositionsModel.find({});
 
   res.json(allPositions);
+});
+
+app.post("/newOrder", (req, res) => {
+  let newOrder = new OrdersModel({
+    name: req.body.name,
+    qty: req.body.qty,
+    price: req.body.price,
+    mode: req.body.mode,
+  });
+
+  newOrder.save();
+
+  res.send("Order saved!");
 });
 
 app.listen(PORT, () => {
