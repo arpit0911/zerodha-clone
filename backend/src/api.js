6a42d665dbd278b25a8f86a191ac9629215f05d9
Arpit Singh <singharpit0911@gmail.com>
2025-08-16 13:23:09 +0530

    make src as function

--- backend/src/api.js
+++ backend/src/api.js
@@ -2,6 +2,6 @@ const serverless = require("serverless-http");
 // Assuming your main Express app file is named 'index.js' or 'app.js'
 // and is located in the 'backend' root.
 // If it's named differently, change 'index' to your file's name.
-const app = require("../src/index");
+const app = require("./index");
 
 module.exports.handler = serverless(app);
